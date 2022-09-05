a7f6e517ba6c2e987d46923ebcb30abcc7a40ad9
Tanay Gaur <gaurtanay6@gmail.com>
2022-09-05 20:23:24 +0530

    intial comit

---
+++ script.js
@@ -0,0 +1,3 @@
+const numberButtons=document.querySelectorAll('[data-number]')
+const operatioinButtons=document.querySelectorAll('[data-operation]')
+const equalsButton=document.querySelector([data-equalsButton])
